e6727f38559fe41417e808de9fd96d9075742cc6
Ryan Nohr <ryan.nohr@gmail.com>
2020-11-26 19:52:10 -0600

    Updated composer

---
+++ vendor/composer/installed.php
@@ -0,0 +1,24 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-main',
+    'version' => 'dev-main',
+    'aliases' => 
+    array (
+    ),
+    'reference' => '2d434519736b26dc2398387fabbfba47b0ad8686',
+    'name' => '__root__',
+  ),
+  'versions' => 
+  array (
+    '__root__' => 
+    array (
+      'pretty_version' => 'dev-main',
+      'version' => 'dev-main',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '2d434519736b26dc2398387fabbfba47b0ad8686',
+    ),
+  ),
+);
