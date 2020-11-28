f2951ecc1561e5e35f6b9878004c41cabdaddaf5
Ryan Nohr <ryan.nohr@gmail.com>
2020-11-27 21:51:39 -0600

    minor api update

--- api.php
+++ api.php
@@ -1,10 +1,4 @@
 <?php
     echo "PHP.api<br />";
-    $request = explode("/", substr(@$_SERVER['PATH_INFO'], 1));
-    if(isset($_ENV['dbUser']))
-    {
-        echo "DB USER SET: ".$_ENV['dbUser']."<br />";
-    }
-    echo json_encode($request);
 
 ?>
