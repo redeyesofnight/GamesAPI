37ff6ca66965d87653375ef4e95b99b1fcee8112
Ryan Nohr <ryan.nohr@gmail.com>
2020-11-27 21:33:52 -0600

    Reworking bones of api for hosting test.

--- api.php
+++ api.php
@@ -1,3 +1,10 @@
 <?php
-    echo "api.php";
-    ?>
+    echo "PHP.api<br />";
+    $request = explode("/", substr(@$_SERVER['PATH_INFO'], 1));
+    if(isset($_ENV['dbUser']))
+    {
+        echo "DB USER SET: ".$_ENV['dbUser']."<br />";
+    }
+    echo json_encode($_ENV);
+
+?>
